ef363762a42afed5b2134c52bf9b225e6abd893f
tuzicomet <leo336570@gmail.com>
2024-08-27 14:57:32 +1200

    tailwindcss and swiper added, along with initial craft setup
    tailwindcss and swiper added. Fields for hero image and slider images, along with the landing page section have been created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/*.twig',
+    './web/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
